617bb9a1391369deb70bafa32ca539c822ec3788
Michal Majer <misa@majer.cz>
2022-01-21 22:45:03 +0100

    cannot do it

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ mod days;
 mod util;
 
 fn main() -> Result<(), &'static str> {
-    //let res = days::day4::part2();
+    let res = days::day4::part2();
 
-    let res = days::solve_challenge(1, 1)?;
     println!("Result: {:?}", res);
 
     Ok(())
